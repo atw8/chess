1004d5a0f6dc3b1d719b92c82afd29695e77c361
atw <2094399853@qq.com>
2019-04-11 15:46:31 +0800

    added the ts-loader library to webpack

--- webpack.debug.config.js
+++ webpack.debug.config.js
@@ -1,30 +1,29 @@
 const path = require('path');
 
 module.exports = {
-    entry: './client_build/app.js',
+    entry: './client_build/app.ts',
     mode : "development",
 
     devtool: 'source-map',
+    watch: true,
+
 
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'client_dist')
     },
+
     resolve: {
-        alias: {
-            pixi: path.join(__dirname, 'node_modules/phaser-ce/build/custom/pixi.js'),
-            phaser: path.join(__dirname, 'node_modules/phaser-ce/build/custom/phaser-split.js'),
-            p2: path.join(__dirname, 'node_modules/phaser-ce/build/custom/p2.js'),
-        }
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
     },
-
-    watch: true,
-
     module: {
         rules: [
-            { test: /pixi\.js$/, loader: 'expose-loader?PIXI' },
-            { test: /phaser-split\.js$/, loader: 'expose-loader?Phaser' },
-            { test: /p2\.js$/, loader: 'expose-loader?p2' },
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.tsx?$/, loader: "ts-loader" }
         ]
     }
+
+
+
 };
--- webpack.release.config.js
+++ webpack.release.config.js
@@ -7,19 +7,5 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'client_dist')
-    },
-    resolve: {
-        alias: {
-            pixi: path.join(__dirname, 'node_modules/phaser-ce/build/custom/pixi.js'),
-            phaser: path.join(__dirname, 'node_modules/phaser-ce/build/custom/phaser-minimum.js'),
-            p2: path.join(__dirname, 'node_modules/phaser-ce/build/custom/p2.js'),
-        }
-    },
-    module: {
-        rules: [
-            { test: /pixi\.js$/, loader: 'expose-loader?PIXI' },
-            { test: /phaser-minimum\.js$/, loader: 'expose-loader?Phaser' },
-            { test: /p2\.js$/, loader: 'expose-loader?p2' },
-        ]
     }
 };
