f936f7b0610f049cc57f2d042e08d99ef3a056b4
atw <2094399853@qq.com>
2019-04-15 00:15:58 +0800

    added LocalStorageManager to manage storing variables in the browser

--- client_build/LocalStorageManager.js
+++ client_build/LocalStorageManager.js
@@ -1,7 +1,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var LocalStorageKeys;
-(function (LocalStorageKeys) {
-    LocalStorageKeys["TOKEN"] = "TOKEN";
-})(LocalStorageKeys = exports.LocalStorageKeys || (exports.LocalStorageKeys = {}));
-//# sourceMappingURL=LocalStorageKeys.js.map
+var LocalStorageManager = /** @class */ (function () {
+    function LocalStorageManager() {
+    }
+    LocalStorageManager.getItemHelper = function (key) {
+        var ret = localStorage.getItem(key);
+        if (ret == null) {
+            return undefined;
+        }
+        return ret;
+    };
+    LocalStorageManager.getGuestTokenStr = function () {
+        return "guestTokenStr";
+    };
+    LocalStorageManager.getGuestToken = function () {
+        return LocalStorageManager.getItemHelper(LocalStorageManager.getGuestTokenStr());
+    };
+    LocalStorageManager.setGuestToken = function (guestToken) {
+        localStorage.setItem(LocalStorageManager.getGuestTokenStr(), guestToken);
+    };
+    return LocalStorageManager;
+}());
+exports.LocalStorageManager = LocalStorageManager;
+/*
+export enum LocalStorageManager {
+    TOKEN = "TOKEN",
+}
+*/ 
+//# sourceMappingURL=LocalStorageManager.js.map
--- client_build/LocalStorageManager.ts
+++ client_build/LocalStorageManager.ts
@@ -1,3 +1,28 @@
-export enum LocalStorageKeys {
+export class LocalStorageManager {
+    private static getItemHelper(key : string):string|undefined{
+        let ret : string | null = localStorage.getItem(key);
+        if(ret == null){
+            return undefined;
+        }
+
+        return ret;
+    }
+
+
+
+    private static getGuestTokenStr():string{
+        return "guestTokenStr";
+    }
+    public static getGuestToken():string|undefined{
+        return LocalStorageManager.getItemHelper(LocalStorageManager.getGuestTokenStr());
+    }
+    public static setGuestToken(guestToken : string){
+        localStorage.setItem(LocalStorageManager.getGuestTokenStr(), guestToken);
+    }
+}
+
+/*
+export enum LocalStorageManager {
     TOKEN = "TOKEN",
 }
+*/
