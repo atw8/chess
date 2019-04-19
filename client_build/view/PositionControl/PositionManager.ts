059096a3b2179f4f3cd011e8574f0f4b90b05d71
atw <2094399853@qq.com>
2019-04-19 17:44:27 +0800

    rewrote PositionManager, the logic is much much simpler

--- client_build/view/PositionControl/PositionManager.ts
+++ client_build/view/PositionControl/PositionManager.ts
@@ -1,71 +1,48 @@
-import {PositionPoints} from "./PositionPoints";
-import {SimpleGame} from "../../app";
+import * as TWEEN from '@tweenjs/tween.js'
 
 
 export class PositionManager {
+    private movingSprites : {sprite : PIXI.DisplayObject,
+        tween : TWEEN.Tween,
+        finishCallback : (() => void) | null,
+        endPosition : PIXI.Point}[];
+    private tweenGroup : TWEEN.Group;
 
-    private m_movingSprites: { "sprite": PIXI.DisplayObject, "action": PositionPoints, "finishCallback": (() => void) | null }[];
 
     private m_delay : number;
 
+
+
     constructor(delay ?: number){
-        this.m_movingSprites = [];
-
-
         if(delay == undefined){
             delay = (1.0 / 60.0) * 1000;
         }
         this.m_delay = delay;
 
-        //let ticker = PIXI.ticker.shared;
+        this.movingSprites = [];
 
-        this.updateMovingSprites(0, null);
+        this.tweenGroup = new TWEEN.Group();
+
+        this.update(0);
     }
 
-    public getLength():number{
-        return this.m_movingSprites.length;
-    }
-
-
-    public addMovingSprite(sprite : PIXI.DisplayObject, action : PositionPoints, finishCallback : (() => void) | null ){
-        this.m_movingSprites.push( { sprite : sprite, action : action, finishCallback : finishCallback});
-    }
-
-    public updateMovingSprites(dt: number, specificSprite: PIXI.DisplayObject | null) {
-        let removeMovingSprites = [];
-        for (let index = 0; index < this.m_movingSprites.length; index++) {
-            let movingSprite = this.m_movingSprites[index];
-
-
-            let sprite = movingSprite["sprite"];
-            let action = movingSprite["action"];
-            let finishCallback = movingSprite["finishCallback"];
-
-            if (specificSprite == null || specificSprite == sprite) {
-                action.tick(dt);
-
-                if (action.isDone()) {
-                    removeMovingSprites.push(index - removeMovingSprites.length);
-                }
-            }
-        }
-
+    public update(dt : number){
         let finishCallbacks: (() => void)[] = [];
 
-        for (let i = 0; i < removeMovingSprites.length; i++) {
-            let index = removeMovingSprites[i];
+        let index = 0;
+        while(index < this.movingSprites.length){
+            let movingSprite = this.movingSprites[index];
 
-            let movingSprite = this.m_movingSprites[index];
+            if (!movingSprite.tween.update(TWEEN.now())) {
+                movingSprite.tween.stop();
+                this.movingSprites.splice(index, 1);
 
-            let sprite = movingSprite["sprite"];
-            let action = movingSprite["action"];
-            let finishCallback = movingSprite["finishCallback"];
-
-            if (finishCallback !== null) {
-                finishCallbacks.push(finishCallback);
+                if(movingSprite.finishCallback != null){
+                    finishCallbacks.push(movingSprite.finishCallback);
+                }
+            }else {
+                index += 1;
             }
-
-            this.m_movingSprites.splice(index, 1);
         }
 
         for (let i = 0; i < finishCallbacks.length; i++) {
@@ -73,37 +50,79 @@ export class PositionManager {
             finishCallback();
         }
 
-        setTimeout(this.updateMovingSprites.bind(this, this.m_delay, null), this.m_delay);
+
+        setTimeout(this.update.bind(this, this.m_delay, null), this.m_delay);
     }
 
+    public moveTo(sprite : PIXI.DisplayObject, finishCallback : (() => void) | null, endPosition : PIXI.Point, speed : number){
+        let startPosition = this.getPosition(sprite);
+        let diffPosition = new PIXI.Point(endPosition.x - startPosition.x, endPosition.y - startPosition.y);
 
-    public isMovingSprite(sprite: PIXI.DisplayObject): boolean {
-        let ret = false;
-        for (let i = 0; i < this.m_movingSprites.length && !ret; i++) {
-            let movingSprite = this.m_movingSprites[i];
+        let duration = Math.sqrt(Math.pow(diffPosition.x, 2) + Math.pow(diffPosition.y, 2))/speed;
 
-            if (sprite === movingSprite.sprite) {
-                ret = true;
+        let lastDelta = 0;
+
+        let tween = new TWEEN.Tween({delta : 0}, this.tweenGroup);
+        tween.to({delta : 1}, duration);
+        tween.onUpdate((o : {delta : number}) => {
+            sprite.position.x += (o.delta - lastDelta)*diffPosition.x;
+            sprite.position.y += (o.delta - lastDelta)*diffPosition.y;
+
+            lastDelta = o.delta;
+        });
+
+        tween.start(TWEEN.now());
+
+        this.movingSprites.push({sprite : sprite, tween : tween, finishCallback : finishCallback, endPosition : endPosition.clone()});
+    }
+
+    public stopMoving(sprite : PIXI.DisplayObject | null){
+        let finishCallbacks : (() => void)[] = [];
+
+        let index = 0;
+        while(index < this.movingSprites.length){
+            let movingSprite = this.movingSprites[index];
+            if(sprite == null || movingSprite.sprite == sprite){
+                movingSprite.tween.end();
+                movingSprite.tween.stop();
+
+                this.movingSprites.splice(index, 1);
+            }else {
+                index += 1;
+            }
+        }
+    }
+
+    public isMoving(sprite : PIXI.DisplayObject | null):boolean{
+        if(sprite == null){
+            return this.movingSprites.length > 0;
+        }
+
+        for(let i = 0; i < this.movingSprites.length; i++){
+            if(this.movingSprites[i].sprite == sprite){
+                return true;
             }
         }
 
-        return ret;
+        return false;
     }
 
 
-    public getEndPosition(sprite : PIXI.DisplayObject):PIXI.Point {
-        let ret : PIXI.Point = sprite.position.clone();
 
-        for(let i = 0; i < this.m_movingSprites.length; i++){
-            let movingSprite = this.m_movingSprites[i];
+    public getPosition(sprite : PIXI.DisplayObject):PIXI.Point{
+        let ret : PIXI.Point | null = null;
 
-            if(sprite == movingSprite.sprite){
-                let deltaPosition = movingSprite.action.getDeltaPosition();
-                ret.x += deltaPosition.x;
-                ret.y += deltaPosition.y;
+        for(let i = this.movingSprites.length - 1; i >= 0 && (ret == null); i--){
+            if(this.movingSprites[i].sprite == sprite){
+                ret = this.movingSprites[i].endPosition;
             }
         }
 
+        if(ret == null){
+            ret = sprite.position;
+        }
+
         return ret;
+
     }
 }
