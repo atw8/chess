66ade129942ace6a01c94ce82364e597eae537a8
atw <2094399853@qq.com>
2019-05-21 21:46:14 +1000

    replaced dependency of m_width, m_height with one dependency m_size

--- client_build/OtherView/SanSprite.ts
+++ client_build/OtherView/SanSprite.ts
@@ -9,10 +9,9 @@ import {SimpleGame} from "../app";
 
 export class SanSprite extends PIXI.Container{
 
-    private sanStr : string;
+    private m_size : number;
 
-    private m_width : number;
-    private m_height : number;
+    private sanStr : string;
 
     private sanText : string;
     private sideType : SideType;
@@ -20,19 +19,18 @@ export class SanSprite extends PIXI.Container{
 
 
     private uiPieceView : PieceView | null;
-    constructor(sanStr : string, sideType : SideType, width : number, height : number){
+    private uiSanText : PIXI.Text;
+
+    constructor(sanStr : string, sideType : SideType, m_size : number){
         super();
 
 
         this.sanStr = sanStr;
         this.sideType = sideType;
-        this.m_width = width;
-        this.m_height = height;
 
+        this.m_size = m_size;
 
         let firstChar = this.sanStr[0];
-
-
 
         let piece : PieceModel | null = ChessEngine.convertFenCharToPieceModel(firstChar);
         if(piece == null || piece.getSideType() == SideType.BLACK){
@@ -44,33 +42,34 @@ export class SanSprite extends PIXI.Container{
         }
 
         if(this.pieceType != PieceType.PAWN){
-            this.uiPieceView = new PieceView({sideType : this.sideType, pieceType : this.pieceType}, this.m_height, this.m_height);
-
-            this.uiPieceView.anchor.set(0.0, 0.5);
-            this.uiPieceView.position.set(0.0, -this.uiPieceView.height*0.15);
+            let sizeConst : number = 1.1;
+            this.uiPieceView = new PieceView({sideType : this.sideType, pieceType : this.pieceType}, this.m_size * sizeConst, this.m_size * sizeConst);
+            this.uiPieceView.anchor.set(0.5, 0.5);
             this.addChild(this.uiPieceView);
         }else {
             this.uiPieceView = null;
         }
 
 
+        let textStyleOptions : PIXI.TextStyleOptions = {};
+        textStyleOptions.fontSize = this.m_size;
+        textStyleOptions.fontFamily = "Helvetica";
+        textStyleOptions.fontWeight = "Bold";
+
         this.uiSanText = new PIXI.Text(this.sanText);
-        let uiSanTextScale : number;
-        uiSanTextScale = this.m_height/this.uiSanText.height;
-        this.uiSanText.scale.set(uiSanTextScale, uiSanTextScale);
-        this.uiSanText.anchor.set(0.0, 0.5);
-        if(this.uiPieceView == null){
-            this.uiSanText.position.set(0.0, 0.0);
-        }else {
-            this.uiSanText.position.set(this.uiPieceView.width, 0.0);
+        this.uiSanText.anchor.set(0.5, 0.5);
+        this.uiSanText.style = new PIXI.TextStyle(textStyleOptions);
+        this.addChild(this.uiSanText);
+
+        if(this.uiPieceView != null){
+            SimpleGame.arrangeHorizontally([this.uiPieceView, this.uiSanText]);
         }
 
-        this.addChild(this.uiSanText);
+
     }
 
 
 
-    private uiSanText : PIXI.Text;
 
 
 }
