60130b874b8e004acf6bc138c571301aaedb8693
atw <2094399853@qq.com>
2019-05-21 21:46:50 +1000

    fixed some problems in PredictSanSprite

--- client_build/OtherView/PredictSanSprite.ts
+++ client_build/OtherView/PredictSanSprite.ts
@@ -2,9 +2,11 @@ import {SideType} from "../../shared/engine/SideType";
 import {SanSprite} from "./SanSprite";
 
 import {SimpleGame} from "../app";
+import * as PIXI from "pixi.js";
 
+let pieceViewScale : number = 0.7;
 
-export class PredictSanSprite extends PIXI.Container{
+export class PredictSanSprite extends PIXI.Graphics{
 
     private m_width : number;
     private m_height : number;
@@ -20,6 +22,7 @@ export class PredictSanSprite extends PIXI.Container{
     private uiPressNode : PIXI.Graphics;
 
 
+
     private pressBtnCallback : (predictSanSprite : PredictSanSprite) => void;
 
     constructor(width : number, height : number, moveTurn : SideType, pressBtnCallback : (predictSanSprite : PredictSanSprite) => void){
@@ -28,40 +31,49 @@ export class PredictSanSprite extends PIXI.Container{
         this.m_height = height;
         this.m_moveTurn = moveTurn;
 
+        this.beginFill(0xFFFFFF, 0.0);
+        this.drawRect(-this.m_width/2, -this.m_height/2, this.m_width, this.m_height);
+
         this.pressBtnCallback = pressBtnCallback;
 
-        {
-            let alphaNode = new PIXI.Graphics();
-            alphaNode.beginFill(0xFFFFFF, 0);
-            alphaNode.drawRect(-this.m_width/2, -this.m_height/2, this.m_width, this.m_height);
-            this.addChild(alphaNode);
-        }
 
 
         this.uiSanSprite = null;
 
         //percentage thing
+        let textStyleOptions : PIXI.TextStyleOptions = {};
+        textStyleOptions.fontSize = this.m_height*pieceViewScale;
+        textStyleOptions.fontFamily = "Helvetica";
+        textStyleOptions.fontWeight = "Bold";
+
         this.uiPercentage = new PIXI.Text("");
+        this.uiPercentage.style = new PIXI.TextStyle(textStyleOptions);
         this.uiPercentage.anchor.set(1.0, 0.5);
-        this.uiPercentage.position.set(this.m_width / 2, 0.0);
-        this.uiPercentage.scale.set(this.m_height / this.uiPercentage.height, this.m_height / this.uiPercentage.height);
+        this.uiPercentage.position.set((this.m_width/2) - 0.5*pieceViewScale*this.m_height, 0.0);
         this.addChild(this.uiPercentage);
 
 
         this.uiPressNode = new PIXI.Graphics();
-        this.uiPressNode.lineStyle(2, 0xA66325, 1);
-        this.uiPressNode.drawRoundedRect(-this.m_width/2, -this.m_height/2, this.m_width, this.m_height, 3);
+        this.uiPressNode.lineStyle(4, 0xA66325, 1);
+        this.uiPressNode.drawRoundedRect(-this.m_width/2, -this.m_height/2, this.m_width, this.m_height, 4);
         this.addChild(this.uiPressNode);
 
-        this.uiPressNode.visible = false;
+        this.uiPressNode.visible = true;
 
 
+        let onUp = () => {
+            this.scale.set(1.0);
+        };
+        let onDown = () => {
+            this.scale.set(0.9);
+        };
+        let onClick = () => {
+            this.pressBtnCallback(this);
+        };
+        //onDown : () => void, onClick : () => void
+
+        SimpleGame.addBtnProperties(this, onUp, onDown, onClick);
         //this.onInputUp.add(this.inputUp.bind(this));
-    }
-
-    public inputUp(){
-        console.debug("inputUp");
-        this.pressBtnCallback(this);
     }
 
     public setIsPredictMove(isPredictMove : boolean){
@@ -104,9 +116,10 @@ export class PredictSanSprite extends PIXI.Container{
         }
 
         if(this.m_sanStr != null && this.m_moveTurn != null){
-            this.uiSanSprite = new SanSprite(this.m_sanStr, this.m_moveTurn, this.m_width, this.m_height);
+            this.uiSanSprite = new SanSprite(this.m_sanStr, this.m_moveTurn, this.m_height*pieceViewScale);
             this.addChild(this.uiSanSprite);
-            this.uiSanSprite.position.set(-this.m_width / 2, 0.0);
+
+            this.uiSanSprite.position.set(-this.m_width/2 + this.uiSanSprite.width/2 + 0.5*pieceViewScale*this.m_height, 0.0);
         }
     }
 
