9481d682d8632de273f929afb5e458e65fc0519b
atw <2094399853@qq.com>
2019-05-21 21:48:03 +1000

    asdf

--- client_build/controller/ControllerAbstract.ts
+++ client_build/controller/ControllerAbstract.ts
@@ -2,17 +2,25 @@ import {MoveClass} from "../../shared/engine/MoveClass";
 import {BoardView} from "../BoardViewLayer/BoardView";
 import {ParentBoardView} from "../BoardViewLayer/ParentBoardView";
 import {LogoLayer} from "../LogoLayer";
+import {PredictPanel} from "../OtherView/PredictPanel";
 
 export interface ControllerAbstract {
-    setParentBoardView(uiParentView : ParentBoardView, uiBoardView : BoardView):void;
+    setParentBoardView(opts : {uiParentView : ParentBoardView,
+        uiBoardView : BoardView,
+        uiPredictPanel : PredictPanel | null,
+        uiPredictBoardView : BoardView | null}):void;
+    //setParentBoardView(uiParentView : ParentBoardView, uiBoardView : BoardView):void;
 
     //Boardview related rubbish
-    notifyMove(moveClass : MoveClass):void;
+    notifyMove(moveClass : MoveClass, uiBoardView : BoardView):void;
 
-    notifyPromote(moveClasses : MoveClass[]):void;
+    notifyPromote(moveClasses : MoveClass[], uiBoardView : BoardView):void;
 
     //Touch related API
     onTouchBegan(point : PIXI.Point):void;
     onTouchMoved(point : PIXI.Point):void;
     onTouchEnded(point : PIXI.Point):void;
+
+    isPredictPanel(): boolean;
+    predictMovePress(isMyMove : boolean, sanStr : string): void;
 }
