c23918db24a5ca5272a573577110a86473d75119
atw <2094399853@qq.com>
2019-06-18 21:32:36 +1000

    asdf

--- server/SocketServerAgent.ts
+++ server/SocketServerAgent.ts
@@ -7,128 +7,85 @@ import {
     ClientServerMessage, ErrorCode, MessageType, OnRoomJoinMessage,
     OnUserLoginGuestMessage, OnRoomMakeMoveMessage, OpRoomJoinMessage,
     OpUserLoginGuestMessage, OpRoomMakeMoveMessage,
-    ServerClientMessage, createMessageFromString, OpRoomMakeVoteMessage
+    ServerClientMessage, createMessageFromString, OpRoomMakeVoteMessage, OpRoomGetRoomStateMessage
 } from "./../shared/MessageTypes";
-import {UserServer} from "./UserServer";
+import {UserAbstract} from "./User/UserAbstract";
+import {UserNormalManager} from "./User/UserNormalManager";
+import {UserStockfishManager} from "./User/UserStockfishManager";
 
 
 export class SocketServerAgent {
-    private io : SocketIO.Server;
-
-    private socketPlayerIdMap : Map<SocketIO.Socket, number>;
-    private playerIdSocketMap : Map<number, SocketIO.Socket>;
+    private playerIdUserAbstractMap : Map<number, UserAbstract>;
 
 
+    private userNormalManager : UserNormalManager;
+    private userStockfishManager : UserStockfishManager;
     private roomServer : RoomServer;
-    private userServer : UserServer;
+
 
 
     constructor(server : http.Server){
+        this.playerIdUserAbstractMap = new Map<number, UserAbstract>();
+
         this.roomServer = new RoomServer(this);
-        this.userServer = new UserServer(this);
 
-        this.socketPlayerIdMap = new Map<SocketIO.Socket, number>();
-        this.playerIdSocketMap = new Map<number, SocketIO.Socket>();
+        this.userNormalManager = new UserNormalManager(this, server);
+        this.userStockfishManager = new UserStockfishManager(this);
 
-        this.io = SocketIO(server);
 
-        this.io.on("connection", this.onConnection.bind(this));
     }
 
-    public onConnection(socket : SocketIO.Socket){
-        console.log("SocketServerAgent.onConnection");
+    public removePlayerIdMap(playerId : number){
+        console.log("SocketServerAgent.removePlayerIdMap");
 
-        socket.on("disconnect", this.onConnectionDisconnect.bind(this, socket));
-        socket.on(MessageType.OpLoginGuest, this.OpLoginGuest.bind(this, socket));
+        this.playerIdUserAbstractMap.delete(playerId);
+    }
+    public addPlayerIdMap(playerId : number, userAbstract : UserAbstract){
+        console.log("SocketServerAgent.addPlayerIdMap");
 
-
-        socket.on(MessageType.OpRoomJoin, this.OpJoinRoom.bind(this, socket));
-        socket.on(MessageType.OpRoomMakeMove, this.OpRoomMakeMove.bind(this, socket));
-
-        socket.on(MessageType.OpRoomMakeVote, this.OpRoomMakeVote.bind(this, socket));
+        this.playerIdUserAbstractMap.set(playerId, userAbstract);
     }
 
-    public onConnectionDisconnect(socket : SocketIO.Socket){
-        console.log("SocketServerAgent.onConnectionDisconnect");
-
-        let playerId : number | undefined = this.socketPlayerIdMap.get(socket);
-        if(typeof playerId !== "undefined"){
-            this.playerIdSocketMap.delete(playerId);
-            this.socketPlayerIdMap.delete(socket);
-        }
-    }
-
-    public emitMessage(socket : SocketIO.Socket | number | undefined, clientServerMessage : ClientServerMessage | null, serverClientMessage : ServerClientMessage){
+    public emitMessage(userAbstract : UserAbstract | number | undefined, clientServerMessage : ClientServerMessage | null, serverClientMessage : ServerClientMessage){
         serverClientMessage.setTimeStamp(Date.now());
         if(clientServerMessage != null){
             serverClientMessage.setRequestId(clientServerMessage.getRequestId());
         }
 
 
-        if(typeof(socket) == "number"){
-            socket = this.playerIdSocketMap.get(socket);
+        if(typeof(userAbstract) == "number"){
+            userAbstract = this.playerIdUserAbstractMap.get(userAbstract);
         }
 
-        if(typeof(socket) != "undefined"){
-            socket.emit(serverClientMessage.getMessageType(), JSON.stringify(serverClientMessage));
+        if(typeof(userAbstract) != "undefined") {
+            userAbstract.emit(serverClientMessage);
         }
     }
 
-    public OpLoginGuest(socket : SocketIO.Socket, message : string){
-        console.log("SocketServerAgent.OpLoginGuest");
+    public OpRoomGetRoomState(playerId : number, opRoomGetRoomStateMsg : OpRoomGetRoomStateMessage){
+        console.log("SocketServerAgent.OpRoomGetRoomState");
 
-        let opUserLoginGuestMsg = createMessageFromString(message, OpUserLoginGuestMessage);
-        if(opUserLoginGuestMsg == null){
-            return;
-        }
-
-        let onUserLoginGuestMsg : OnUserLoginGuestMessage = new OnUserLoginGuestMessage({guestToken : "", playerId : 0, roomIds : []});
-
-        this.userServer.guestLogin(opUserLoginGuestMsg, onUserLoginGuestMsg);
-
-        let playerId = onUserLoginGuestMsg.playerId;
-
-        this.playerIdSocketMap.set(playerId, socket);
-        this.socketPlayerIdMap.set(socket, playerId);
-
-        onUserLoginGuestMsg.roomIds = this.roomServer.getRoomIdsForPlayerId(playerId);
-
-        this.emitMessage(socket, opUserLoginGuestMsg, onUserLoginGuestMsg);
+        this.roomServer.getRoomState(playerId, opRoomGetRoomStateMsg);
     }
 
-    public OpJoinRoom(socket : SocketIO.Socket, message : string){
-        console.log("SocketServerAgent.OpJoinRoom");
-        let opRoomJoinMsg = createMessageFromString(message, OpRoomJoinMessage);
-        if(opRoomJoinMsg == null){
-            return;
-        }
+    public OpRoomJoin(playerId : number, opRoomJoinMsg : OpRoomJoinMessage){
+        console.log("SocketServerAgent.OpRoomJoin");
 
 
-        let playerId = <number>this.socketPlayerIdMap.get(socket);
         this.roomServer.joinRoom(playerId, opRoomJoinMsg);
     }
 
-    public OpRoomMakeMove(socket : SocketIO.Socket, message : string){
+    public OpRoomMakeMove(playerId : number, opRoomMakeMoveMsg : OpRoomMakeMoveMessage){
         console.log("SocketServerAgent.OpRoomMakeMove");
-        let opRoomMakeMoveMsg = createMessageFromString(message, OpRoomMakeMoveMessage);
-        if(opRoomMakeMoveMsg == null){
-            return;
-        }
 
 
-
-        let playerId = <number>this.socketPlayerIdMap.get(socket);
         this.roomServer.makeMove(playerId, opRoomMakeMoveMsg);
     }
 
-    public OpRoomMakeVote(socket : SocketIO.Socket, message : string){
+    public OpRoomMakeVote(playerId : number, opRoomMakeVoteMsg : OpRoomMakeVoteMessage){
         console.log("SocketServerAgent.OpRoomMakeVote");
-        let opRoomMakeVoteMsg = createMessageFromString(message, OpRoomMakeVoteMessage);
-        if(opRoomMakeVoteMsg == null){
-            return;
-        }
 
-        let playerId = <number>this.socketPlayerIdMap.get(socket);
+
         this.roomServer.makeVote(playerId, opRoomMakeVoteMsg);
     }
 }
